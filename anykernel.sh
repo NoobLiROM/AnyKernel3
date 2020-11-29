9c1aacbf77b8a1ad60f4b44f96d30dd353743df1
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-11-29 10:12:05 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Li-Kernel
+kernel.string=ALi Kernel TESTING
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=beryllium
-device.name2=PocoF1
-device.name3=polaris
-device.name4=dipper
-device.name5=equuleus
-supported.versions=11,11.0
+device.name1=polaris
+device.name2=dipper
+device.name3=beryllium
+supported.versions=10,11
 supported.patchlevels=
 '; } # end properties
 
@@ -39,26 +37,5 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
 write_boot;
 ## end install
-
